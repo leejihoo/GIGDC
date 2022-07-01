861be159170d9e2ddefc8280c241a54cca55d9f5
JeongJiwon <eetr3295@gmail.com>
2022-07-01 20:44:51 +0900

    상어 scene, test 스크립트

--- Assets/Script/Shark/Test.cs
+++ Assets/Script/Shark/Test.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Test : MonoBehaviour
 {
     public GameObject[] waves;
+    public GameObject shark;
 
     public void Click() {
         StartCoroutine(ClickAnim());
@@ -15,7 +16,8 @@ public class Test : MonoBehaviour
         var wait = new WaitForSeconds(0.3f);
         for(int i = 0; i < 5; i++) {
             GameObject wave = GameObject.Instantiate(waves[i%2]) as GameObject;
-            wave.transform.position = new Vector3(0,0,0);
+            wave.transform.rotation = shark.transform.rotation;
+            wave.transform.position = shark.transform.position;
 
             yield return wait;
         }
