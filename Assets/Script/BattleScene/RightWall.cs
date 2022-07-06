e477420c941bb9de162bcaea8e693032fd1e2def
JeongJiwon <eetr3295@gmail.com>
2022-07-07 01:18:44 +0900

    RightWall에 총알 맞으면 총알 사라지게 구현. 나중에 BattleScene에 있는 RightWall을 추가한 RightWall.Prefab으로 교체해야함.

---
+++ Assets/Script/BattleScene/RightWall.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightWall : MonoBehaviour
+{
+  private void OnTriggerEnter2D(Collider2D other) {
+    if(other.CompareTag("Bullet")) Destroy(other.gameObject);
+  }
+}
